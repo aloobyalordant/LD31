e3aa1a6559fdffd2b614925833429be5c1439e4f
Mark Jones <aloobyalordant@users.noreply.github.com>
2014-12-13 00:59:34 +0000

    Add sound references back in - does it work now?

--- SoundManager.java
+++ SoundManager.java
@@ -13,16 +13,16 @@ public class SoundManager {
 			int choice = ran.nextInt(4);
 			switch (choice){
 				case 0:
-//					soundFileList.add("Sounds/151232__owlstorm__hard-female-footstep-1.wav");
+					soundFileList.add("Sounds/151232__owlstorm__hard-female-footstep-1.wav");
 					break;
 				case 1:
-//					soundFileList.add("Sounds/151238__owlstorm__hard-female-footstep-2.wav");
+					soundFileList.add("Sounds/151238__owlstorm__hard-female-footstep-2.wav");
 					break;
 				case 2:
-//					soundFileList.add("Sounds/151237__owlstorm__hard-female-footstep-3.wav");
+					soundFileList.add("Sounds/151237__owlstorm__hard-female-footstep-3.wav");
 					break;
 				default: 
-//					soundFileList.add("Sounds/151222__owlstorm__hard-female-footstep-4.wav");
+					soundFileList.add("Sounds/151222__owlstorm__hard-female-footstep-4.wav");
 					break;
 				// footstep sounds from http://www.freesound.org/people/OwlStorm/packs/9344/  - Natalie Kirk / Esper Studio. 
 			}
